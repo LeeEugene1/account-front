e2f2cddc6509abe22382c586d89ddde748a50eea
eugene <uwm1004@gmail.com>
2022-02-02 22:26:40 +0900

    login css

--- store/modal.js
+++ store/modal.js
@@ -5,10 +5,11 @@ export const state = () =>({
 export const mutations = {
     SET_LOGIN_MODAL_OPEN(state){
         state.login.show = true
-    },
-    SET_LOGIN_MODAL_DIRECT_LOGIN(state){
         state.login.directLogin = true
     },
+    // SET_LOGIN_MODAL_DIRECT_LOGIN(state){
+    //     state.login.directLogin = true
+    // },
     SET_LOGIN_MODAL_CLOSE(state){
         state.login = {
             show:false, 
